844d330d3ea1b5a01b027038ecaf8384dc24ec9e
DPrakash <divyaprakash4u@outlook.com>
2021-07-21 22:35:21 +0200

    Creating a pygame window and responding to user input

---
+++ alien_invasion.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 19 23:31:54 2021
+
+@author: DIVYAPRAKASH
+"""
+
+import sys
+import pygame
+
+class AlienInvasion:
+    """Overall class to manage game assets and resources"""
+    def __init__(self):
+        """Initialize the game and generate resources"""
+        pygame.init()
+        self.screen=pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+        
+    def run_game(self):
+        """Start the main loop of the game"""
+        while True:
+            #watch for keyboard and mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                    
+            #Make the most recently drawn screen visible
+            pygame.display.flip()
+            
+if __name__ == '__main__':
+    #Make a game instance and run the game
+    ai = AlienInvasion()
+    ai.run_game()
+    
+        
+        
