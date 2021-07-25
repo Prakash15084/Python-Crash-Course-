a8df246ee548656c2c2f1fe051b99870d155f308
DPrakash <divyaprakash4u@outlook.com>
2021-07-25 22:35:18 +0200

    New settings file created

---
+++ settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 25 22:24:18 2021
+
+@author: DIVYAPRAKASH
+"""
+
+class Settings():
+    """A class to store all settings for Alien Invasion"""
+    def __init__(self):
+        """Initialize the game settings"""
+        #Screen settings
+        self.screen_width= 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
+        
+        
